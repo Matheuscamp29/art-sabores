6e39cff6b5963a95f0855af7af9d6c15389be028
LuisHenriqueDC301 <luishenrique30102005@yahoo.com>
2025-05-28 14:14:54 -0300

    Fornecedor CRUD

--- api/Program.cs
+++ api/Program.cs
@@ -38,6 +38,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("AllowAll");
 
+//Fornecedor CRUD
+
+//Incluir Fornecedor
 app.MapPost("/api/v1/fornecedor", async (Fornecedor fornecedor, AppDbContext dao) =>
 {
     dao.Fornecedores.Add(fornecedor);
@@ -46,8 +49,37 @@ app.MapPost("/api/v1/fornecedor", async (Fornecedor fornecedor, AppDbContext dao
     return Results.Created($"/api/v1/fornecedor/{fornecedor.Id}", fornecedor);
 });
 
+//Mostra os Fornecedores
 app.MapGet("/api/v1/getFornecedores", async (AppDbContext dao) => 
     await dao.Fornecedores.ToListAsync());
+
+//Atualizar Fornecedor
+app.MapPut("/api/v1/updateFornecedor/{id}", async (int id, Fornecedor input, AppDbContext dao) =>
+{
+    var fornecedor = await dao.Fornecedores.FindAsync(id);
+    if (fornecedor is null)
+        return Results.NotFound("Fornecedor não encontrado.");
+
+    // Atualize as propriedades que deseja modificar
+    fornecedor.nome = input.nome;
+
+    await dao.SaveChangesAsync();
+    return Results.Ok(fornecedor);
+});
+
+//Delete Fornecedor
+app.MapDelete("/api/v1/deleteFornecedor/{id}", async (int id, AppDbContext dao) =>
+{
+    var fornecedor = await dao.Fornecedores.FindAsync(id);
+    if (fornecedor is null)
+        return Results.NotFound("Fornecedor não encontrado.");
+
+    dao.Fornecedores.Remove(fornecedor);
+    await dao.SaveChangesAsync();
+
+    return Results.Ok($"Fornecedor com ID {id} deletado com sucesso.");
+});
+
 
 
 // Configure the HTTP request pipeline.
