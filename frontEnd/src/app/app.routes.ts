c34ad1b5ef3b4477c0094d549d54a0cd895249ec
Vinicius Cesar Dascenção Dias <1453563@sga.pucminas.br>
2025-05-29 16:47:05 -0300

    frontendEd

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FornecedorComponent } from './pages/fornecedor/fornecedor.component';
+import { MateriaPrimaComponent } from './pages/materia-prima/materia-prima.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'fornecedor', pathMatch: 'full' },
-  { path: 'fornecedor', component: FornecedorComponent },
+  { path: 'fornecedor', component: FornecedorComponent }, 
+  { path: 'materiaPrima', component: MateriaPrimaComponent }
 ];
 
 
---
+++ frontEnd/src/app/pages/materia-prima/materia-prima.component.spec.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-materia-prima',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HttpClientModule,   // Aqui importa o HttpClientModule
+    ReactiveFormsModule,
+  ],
+  templateUrl: './materia-prima.component.html',
+  styleUrls: ['./materia-prima.component.css']
+})
+
+export class MateriaPrimaComponent {
+  mostrarFormulario = false;
+  materiaForm: FormGroup;
+  materias: { nome: string; estoque: number }[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.materiaForm = this.fb.group({
+      nome: ['', Validators.required],
+      estoque: [0, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  toggleFormulario() {
+    this.mostrarFormulario = !this.mostrarFormulario;
+  }
+
+  salvarMateria() {
+    if (this.materiaForm.valid) {
+      this.materias.push(this.materiaForm.value);
+      this.mostrarFormulario = false;
+      this.materiaForm.reset({ nome: '', estoque: 0 });
+    }
+  }
+
+  cancelarFormulario() {
+    this.mostrarFormulario = false;
+    this.materiaForm.reset({ nome: '', estoque: 0 });
+  }
+}
