b068cbdd072d413a148a06ccd6efef705d3e7726
LuisHenriqueDC301 <luishenrique30102005@yahoo.com>
2025-06-03 18:22:48 -0300

    task

--- api/Models/VendaFornecedorMateria.cs
+++ api/Models/VendaFornecedorMateria.cs
@@ -5,8 +5,6 @@ namespace Art_Sabores.Models
     public class VendaFornecedorMateria
     {
         public int Id { get; set; }
-        [ForeignKey("ItemProduto")]
-        public int ItemProdutoId { get; set; }
         public String NFSe { get; set; }
         
     }
