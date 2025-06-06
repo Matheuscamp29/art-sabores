72801f31eeb1f2cb2283fcb8e954adb959d3bb5d
Matheuscamp29 <mdocampello2909@gmail.com>
2025-06-06 08:35:00 -0300

    mais front

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FornecedorComponent } from './pages/fornecedor/fornecedor.component';
-import { MateriaPrimaComponent } from './pages/materia-prima/materia-prima.component';
 import { ClienteComponent } from './pages/cliente/cliente.component';
+import { EstoqueComponent } from './pages/estoque/estoque.component';
+
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'fornecedor', pathMatch: 'full' },
   { path: 'fornecedor', component: FornecedorComponent }, 
-  { path: 'materiaPrima', component: MateriaPrimaComponent },
+  { path: 'estoque', component: EstoqueComponent },
   { path: 'cliente', component: ClienteComponent } 
 ];
 
