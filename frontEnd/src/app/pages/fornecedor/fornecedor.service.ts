2352d11dd6ce9329de8ad16f52411727b868acb6
Matheuscamp29 <mdocampello2909@gmail.com>
2025-06-10 17:10:04 -0300

    asd

--- frontEnd/src/app/pages/fornecedor/fornecedor.service.ts
+++ frontEnd/src/app/pages/fornecedor/fornecedor.service.ts
@@ -1,4 +1,3 @@
-// services/fornecedor.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -7,11 +6,24 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FornecedorService {
-  private apiUrl = ''; // Substitua pela sua URL
 
-  constructor(private http: HttpClient) {}
+  private apiUrl = '/api/v1/fornecedor';
+
+  constructor(private http: HttpClient) { }
 
   getFornecedores(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(`${this.apiUrl}`);
+  }
+
+  createFornecedor(fornecedor: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}`, fornecedor);
+  }
+
+  updateFornecedor(id: number, fornecedor: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/updateFornecedor/${id}`, fornecedor);
+  }
+
+  deleteFornecedor(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/deleteFornecedor/${id}`);
   }
 }
