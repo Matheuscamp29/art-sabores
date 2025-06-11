c4636ddf8270188140fbcde2f23bcf1a679d8d7d
LuisHenriqueDC301 <luishenrique30102005@yahoo.com>
2025-06-11 11:05:02 -0300

    Feat: Salgado Crud

--- frontEnd/src/app/pages/salgado/salgado.component.ts
+++ frontEnd/src/app/pages/salgado/salgado.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { SalgadoService } from './salgado.service'; // <- AQUI
+import { SalgadoService } from './salgado.service';
 import { HeaderComponent } from '../header/header.component';
 import { CommonModule } from '@angular/common';
 
@@ -14,16 +14,17 @@ import { CommonModule } from '@angular/common';
     ReactiveFormsModule,
     HttpClientModule,
     HeaderComponent 
-  ]
+  ]
 })
 export class SalgadoComponent implements OnInit {
   salgados: any[] = [];
   mostrarFormulario = false;
   salgadoForm!: FormGroup;
+  salgadoEditando: any = null; // <- usado para edição
 
   constructor(
     private http: HttpClient,
-    private salgadoService: SalgadoService, // <- AQUI
+    private salgadoService: SalgadoService,
     private fb: FormBuilder
   ) {}
 
@@ -35,29 +36,64 @@ export class SalgadoComponent implements OnInit {
   toggleFormulario() {
     this.mostrarFormulario = !this.mostrarFormulario;
     if (this.mostrarFormulario && !this.salgadoForm) {
-      this.salgadoForm = this.fb.group({
-        nome: ['', Validators.required],
-        preco: [0, [Validators.required, Validators.min(0)]],
-        estoque: [0, [Validators.required, Validators.min(0)]]
-      });
+      this.inicializarFormulario();
     }
   }
 
-  salvarSalgado() {
-    if (this.salgadoForm.valid) {
-      const novoSalgado = this.salgadoForm.value;
+  inicializarFormulario() {
+    this.salgadoForm = this.fb.group({
+      nome: ['', Validators.required],
+      preco: [0, [Validators.required, Validators.min(0)]],
+      estoque: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
 
-      this.salgadoService.createSalgado(novoSalgado)
+  salvarSalgado() {
+    if (this.salgadoForm.invalid) return;
+
+    const dados = this.salgadoForm.value;
+
+    if (this.salgadoEditando) {
+      this.salgadoService.updateSalgado(this.salgadoEditando.id, dados)
+        .subscribe(response => {
+          const index = this.salgados.findIndex(s => s.id === this.salgadoEditando.id);
+          if (index > -1) this.salgados[index] = response;
+          this.cancelarFormulario();
+        });
+    } else {
+      this.salgadoService.createSalgado(dados)
         .subscribe(response => {
           this.salgados.push(response);
-          this.mostrarFormulario = false;
-          this.salgadoForm.reset();
+          this.cancelarFormulario();
+        });
+    }
+  }
+
+  editarSalgado(salgado: any) {
+    this.salgadoEditando = salgado;
+    this.mostrarFormulario = true;
+
+    if (!this.salgadoForm) this.inicializarFormulario();
+
+    this.salgadoForm.patchValue({
+      nome: salgado.nome,
+      preco: salgado.preco,
+      estoque: salgado.estoque
+    });
+  }
+
+  excluirSalgado(id: number) {
+    if (confirm('Tem certeza que deseja excluir?')) {
+      this.salgadoService.deleteSalgado(id)
+        .subscribe(() => {
+          this.salgados = this.salgados.filter(s => s.id !== id);
         });
     }
   }
 
   cancelarFormulario() {
     this.mostrarFormulario = false;
+    this.salgadoEditando = null;
     if (this.salgadoForm) this.salgadoForm.reset();
   }
 }
