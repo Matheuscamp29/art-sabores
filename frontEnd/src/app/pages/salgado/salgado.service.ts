51fc36366ed566f53fe207e6f5ca5ccf996939a9
Matheuscamp29 <mdocampello2909@gmail.com>
2025-06-11 10:33:57 -0300

    salgadao

---
+++ frontEnd/src/app/pages/salgado/salgado.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalgadoService {
+  private apiUrl = 'https://localhost:32771/api/v1';
+
+  constructor(private http: HttpClient) {}
+
+  getSalgados(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/getSalgados`);
+  }
+
+  createSalgado(salgado: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/salgado`, salgado);
+  }
+
+  updateSalgado(id: number, salgado: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/updateSalgado/${id}`, salgado);
+  }
+
+  deleteSalgado(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/deleteSalgado/${id}`);
+  }
+}
